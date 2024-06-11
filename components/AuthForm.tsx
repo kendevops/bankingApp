1788fba9a310ad11239b1e822ced324b7d58edb5
Kenneth Aladi <kendevops@gmail.com>
2024-06-11 10:49:45 +0100

    implemented structure for the signup and signIn

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,8 +20,10 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/router";
 
 const AuthForm = ({ type }: { type: string }) => {
+    const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -36,22 +38,21 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit = async (data: z.infer<typeof formSchema>) => {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true);
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
       try {
           //Signup with Appwrite & create plaid link token
           if (type === 'sign-up') {
-              const userData = {
-                  firstName: data.firstName,
-                  lastName: data.lastName,
-
-
-              }
+            //   const newUser = await signUp(data);
+            //   setUser(newUser);
           }
           if (type === 'sign-in') {
-              
+            //   const response = await SignIn({ email: data.email, password: data.password })
+            //   if (response) {
+            //       router.push('/')
+            //   }
           }
       } catch(error) {
           console.log(error)
